90d13f620e9dd131045fa32116d103618ba7fbd0
comdo-santhosh <sanss2620818@gmail.com>
2024-02-02 19:43:17 +0530

    fix : extra loop added

--- hello.java
+++ hello.java
@@ -3,7 +3,10 @@ public static void main(String[] args) {
     for(int i = 0 ; i  < 10; i++){
         System.out.println(i);
     }
-    System.out.println("Helloworld");   
-
+    int i = 0;
+    while ( i < 10) {
+        i++;
+        System.out.println(i);
+    }
 }
 }
