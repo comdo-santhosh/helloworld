c04f9b40e5403bf9c5ce593eea4084b9fc2d5eea
comdo-santhosh <sanss2620818@gmail.com>
2024-02-02 20:03:33 +0530

    fix:some coded added

--- sample.java
+++ sample.java
@@ -4,5 +4,8 @@ public class sample {
 public static void main(String[] args) {
     int arr[] = {1,4,5,6,5,7,6,7,3,4,8,6};
     System.out.println(Arrays.toString(arr));
+    for (int i : arr) {
+        System.out.println(i);
+    }
 }    
 }
